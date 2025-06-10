f12d2387c86a413b76f6c98cc707b216303c7182
KrisYangPO <kristoff0109@gmail.com>
2025-06-10 14:41:47 +0800

    establish spring

---
+++ BestRally/src/main/java/match/BestRallyApplication.java
@@ -0,0 +1,13 @@
+package match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestRallyApplication.class, args);
+	}
+
+}
